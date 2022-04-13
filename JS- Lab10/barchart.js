da92d94f370e5242840fd6226e3e50931f424479
Anuoluwapo7 <98867047+Anuoluwapo7@users.noreply.github.com>
2022-04-12 22:30:29 -0400

    Add files via upload

---
+++ JS- Lab10/barchart.js
@@ -0,0 +1,41 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['America', 'East Asia & Pacific', 'Europe & Central Asia', 'Middle East & North Africa', 'South Asia', 'Sub-Saharan Africa'],
+        datasets: [{
+            label: 'GDP Per Capita',
+            data: [18791.13, 14480.30, 20818.45, 13856.00, 2505.17, 2569.00],
+            backgroundColor: [
+                '#ff0800'
+            ]
+        },
+       
+            
+        ]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: 'GDP Per Capita By Region',
+                font: {
+                    size: 18
+                }
+            },
+
+            subtitle: {
+                display: true,
+                text: 'The Gross Domestic Product (GDP) indicates the vitality of national and world-wide economies. The following regions are reflected in the visualization.'
+            }
+        },
+        scales: {
+            y: {
+                beginAtZero: true,
+            
+            },
+           
+        },
+    },
+});
